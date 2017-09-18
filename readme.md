0317d65cad8062d01cfeaea1b33fef5823ec2cb9
smoliver <samuelolive21@gmail.com>
2017-09-18 18:57:30 -0400

    vanilla ready to go

--- readme.md
+++ readme.md
@@ -1,12 +1,20 @@
 # Welcome to *Feed Your Family with Javascript* 
 ## An Introduction to Javascript, ES6, and frontend for Object Oriented developers
 
-### To get started download [node.js](https://nodejs.org/en/) 
+### Getting started
+1. Download [node.js](https://nodejs.org/en/) 
+2. If you don't already have a preffered web-dev editor check out either
+    * [Sublime Text](https://www.sublimetext.com/3) 
+    * [Atom](https://atom.io/)
+3. Clone this repository 
+```git clone https://github.com/smoliver/intro-to-frontend```
+4. Download [chrome](https://www.google.com/chrome/browser/desktop/index.html);
 
-#### Topics
+### Topics
 1. [Vanilla Javascript & ES6](./vanilla)
-    * Variable and `this` scoping in JS
-    * Truthiness
+    * JS Variables [var](https://goo.gl/EQFfp9) [let](https://goo.gl/cRfCme) [const](https://goo.gl/7YR1W9)
+    * [this](https://goo.gl/pBvU9e) scoping in JS
+    * JS Truthiness
     * DOM
     * ES6
     * SASS
--- vanilla/dist/classic/index.js
+++ vanilla/dist/classic/index.js
@@ -4,7 +4,7 @@ console.group ();
 console.log ('The `this` keyword is one of the largest (and most confusing) differences between JS and C based languages');
 console.log ('In Javascript functions are treated as first class objects');
 console.log ('They can be passed into functions, set as variables, and returne values');
-console.log ('As a result, certain types of function expressions will edit the scope of `this`');
+console.log ('As a result, certain types of functions and modes will edit the scope of `this`');
 console.groupEnd ();
 
 
@@ -12,33 +12,17 @@ console.group ("`this` unwrapped in vanilla js:");
 console.log (this);
 console.groupEnd ();
 
+// Function expressions create a new context in strict mode
 (function () {
   console.group ("`this` wrapped in a function expression");
   console.log (this);
   console.groupEnd ();
 })();
 
+// ES6 arrow functions never create new context 
 (() => {
   console.group ("`this` wrapped in es6 arrow function call");
   console.log (this);
   console.groupEnd ();
 })();
-
-var anUnnammedFunction = function () {
-  console.group ("")
-
-}).call (this);
-
-function aNamedFunction (message) {
-  console.group (message)
-  console.log (this);
-  console.groupEnd ();
-}
-
-class ThisTest {
-
-  test () {
-    console.log (this);
-  }
-}
 
